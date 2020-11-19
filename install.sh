35f237c55aa0ef595f57147976a2ae97cd68a0a5
kin3303 <48729027+kin3303@users.noreply.github.com>
2020-11-19 13:04:10 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Create swapfile 4G
+fallocate -l 4G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+echo '/swapfile  none  swap  sw  0 0' >>/etc/fstab
+swapon -a
+    
+# docker install
+curl -fsSL get.docker.com -o get-docker.sh
+sh get-docker.sh
+rm -rf ./get-docker.sh
+
+# dcs install
+curl -sL bit.ly/ralf_dcs -o ./dcs
+chmod 755 ./dcs
+mv ./dcs /usr/bin/dcs
+
+# docker-compose install
+curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -sfT /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# etc install
+apt-get install -y make
+apt-get install -y gnupg2 pass
+
+# install multitail
+apt-get install multitail -y
